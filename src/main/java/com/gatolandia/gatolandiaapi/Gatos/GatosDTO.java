a90014762ef3fbb22c768a12a6d3f0e57fdf1ec0
Matheus Nísholas <nisholas.workspace@outlook.com>
2025-10-04 10:37:53 -0300

    feat: Estrutura do DTO e Mapper para os gatos pronta

--- src/main/java/com/gatolandia/gatolandiaapi/Gatos/GatosDTO.java
+++ src/main/java/com/gatolandia/gatolandiaapi/Gatos/GatosDTO.java
@@ -1,4 +1,22 @@
 package com.gatolandia.gatolandiaapi.Gatos;
 
+import com.gatolandia.gatolandiaapi.Donos.DonosModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GatosDTO {
+
+    private long id;
+    private String nome;
+    private String cor;
+    private String raca;
+    private int idade;
+    private boolean castrado;
+    private DonosModel donos;
+
+
 }
--- src/main/java/com/gatolandia/gatolandiaapi/Gatos/GatosMapper.java
+++ src/main/java/com/gatolandia/gatolandiaapi/Gatos/GatosMapper.java
@@ -1,4 +1,31 @@
 package com.gatolandia.gatolandiaapi.Gatos;
 
+import com.gatolandia.gatolandiaapi.Donos.DonosDTO;
+import com.gatolandia.gatolandiaapi.Donos.DonosModel;
+
 public class GatosMapper {
+
+    public GatosModel map(GatosDTO gatosDTO) {
+        GatosModel gatosModel = new GatosModel();
+        gatosModel.setId(gatosDTO.getId());
+        gatosModel.setNome(gatosDTO.getNome());
+        gatosModel.setCor(gatosDTO.getCor());
+        gatosModel.setCor(gatosDTO.getCor());
+        gatosModel.setRaca(gatosDTO.getRaca());
+        gatosModel.setCastrado(gatosDTO.isCastrado());
+
+        return gatosModel;
+    }
+
+    public GatosDTO map(GatosModel gatosModel) {
+        GatosDTO gatosDTO = new GatosDTO();
+        gatosDTO.setId(gatosModel.getId());
+        gatosDTO.setNome(gatosModel.getNome());
+        gatosDTO.setCor(gatosModel.getCor());
+        gatosDTO.setCor(gatosModel.getCor());
+        gatosDTO.setRaca(gatosModel.getRaca());
+        gatosDTO.setCastrado(gatosModel.isCastrado());
+
+        return gatosDTO;
+    }
 }
