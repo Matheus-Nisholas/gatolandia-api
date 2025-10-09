2429af8f0aea84154a9c8188f7ce10d895068f10
Matheus Nísholas <nisholas.workspace@outlook.com>
2025-10-09 20:57:22 -0300

    refactor(donos): Migra DonosService para usar a interface MapStruct

--- src/main/java/com/gatolandia/gatolandiaapi/Donos/DonosService.java
+++ src/main/java/com/gatolandia/gatolandiaapi/Donos/DonosService.java
@@ -1,39 +1,57 @@
 package com.gatolandia.gatolandiaapi.Donos;
 
+import com.gatolandia.gatolandiaapi.Gatos.GatosDTO;
+import com.gatolandia.gatolandiaapi.Gatos.GatosModel;
+import com.gatolandia.gatolandiaapi.Gatos.GatosRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class DonosService {
 
+    private DonosMapper donosMapper;
     private DonosRepository donosRepository;
 
-    public DonosService(DonosRepository donosRepository) {
+    public DonosService(DonosRepository donosRepository, DonosMapper donosMapper) {
         this.donosRepository = donosRepository;
+        this.donosMapper = donosMapper;
     }
 
-    public List<DonosModel> exibirDonos(){
-         return donosRepository.findAll();
+    public List<DonosDTO>  listarDonos() {
+        List<DonosModel> gatos = donosRepository.findAll();
+        return gatos.stream()
+                .map(donosMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
-    public DonosModel exibirDonoPorId(long id){
-        return donosRepository.findById(id).orElse(null);
+    public DonosDTO exibirPorId(long id) {
+        Optional<DonosModel> exibirPorId = donosRepository.findById(id);
+        return exibirPorId.map(donosMapper::toDTO).orElse(null);
     }
 
-    public void deletarDonosPorId(long id){
+    public DonosDTO adicionarDonos(DonosDTO donosDTO) {
+
+        DonosModel dono = donosMapper.toModel(donosDTO);
+        dono = donosRepository.save(dono);
+        return donosMapper.toDTO(dono);
+    }
+
+    public void excluirDonosPorId(long id) {
         donosRepository.deleteById(id);
     }
 
-    public DonosModel adicionarDonos(DonosModel donosModel){
-        return donosRepository.save(donosModel);
+    public DonosDTO editarDonos(DonosDTO donosDTO, Long id) {
+        Optional<DonosModel> donosExistentes = donosRepository.findById(id);
+        if (donosExistentes.isPresent()) {
+            DonosModel donosEditado = donosMapper.toModel(donosDTO);
+            donosEditado.setId(id);
+            donosEditado = donosRepository.save(donosEditado);
+            return donosMapper.toDTO(donosEditado);
+        }
+        return null;
     }
 
-    public DonosModel editarDono(DonosModel donoAtualizado, long id){
-    if (donosRepository.existsById(id)){
-        donoAtualizado.setId(id);
-        return donosRepository.save(donoAtualizado);
-    } else {
-        return null;}
-    }
 }
